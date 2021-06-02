a9563d75aa93d210ed2af99d5415151be506cea5
kajsss <kajsss@gmail.com>
2021-06-02 22:59:57 +0900

    modify rabbitconfig

--- eshop-backend/src/main/java/com/samsungsds/eshop/config/RabbitConfig.java
+++ eshop-backend/src/main/java/com/samsungsds/eshop/config/RabbitConfig.java
@@ -7,10 +7,12 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,6 +27,19 @@ public class RabbitConfig {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
+    }
+
+    @Bean
+    ConnectionFactory connectionFactory(
+            @Value("${spring.rabbitmq.host}") String server,
+            @Value("${spring.rabbitmq.port}") int port,
+            @Value("${spring.rabbitmq.username}") String username,
+            @Value("${spring.rabbitmq.password}") String password) {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(
+                server, port);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        return connectionFactory;
     }
 
     @Bean
--- eshop-shippingservice/src/main/java/com/samsungsds/eshop/rabbit/RabbitConfig.java
+++ eshop-shippingservice/src/main/java/com/samsungsds/eshop/rabbit/RabbitConfig.java
@@ -4,10 +4,12 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -39,6 +41,19 @@ public class RabbitConfig {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
+    }
+
+    @Bean
+    ConnectionFactory connectionFactory(
+            @Value("${opentracing.rabbitmq.host}") String server,
+            @Value("${opentracing.rabbitmq.port}") int port,
+            @Value("${opentracing.rabbitmq.username}") String username,
+            @Value("${opentracing.rabbitmq.password}") String password) {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(
+                server, port);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        return connectionFactory;
     }
 
     @Bean
